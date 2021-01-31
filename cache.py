bedbdc3716539f90f3635b86813bd8250a95ddb7
Guilherme Vinicius <guilhermevini@outlook.com>
2021-01-31 14:36:09 -0300

    adjust python3

--- cache.py
+++ cache.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function
-
 import os
 import boto3
 import time
 
 client = boto3.client('cloudfront')
+
 
 def display_title_bar():
     os.system('clear')
@@ -18,12 +15,13 @@ def display_title_bar():
     print("\t**********************************************")
     print("\n")
 
+
 def get_user_choice():
     print("\t[1] Listar Distribuições.")
     print("\t[q] Sair.")
     print("\n")
 
-    return raw_input("\tEscolha uma opção: ")
+    return input("\tEscolha uma opção: ")
 
 
 def show_distributions():
@@ -31,19 +29,24 @@ def show_distributions():
 
     response = client.list_distributions()
     dlist = response['DistributionList']
-    
+    lines = dlist['Quantity']
+
+    if (lines <= 0):
+        print("\tNenhuma distribuição encontrada.")
+        return
+
     dists = []
-    count=0
+    count = 0
 
     for dist in dlist['Items']:
         id = dist['Id']
         print("\t[%s] " % count + id + " (" + dist['Status'] + ")")
-        print("\t"+ dist['DomainName'])
+        print("\t" + dist['DomainName'])
 
         dists.insert(count, id)
-        count +=1
+        count += 1
 
-        if ('Items' in dist['Aliases']): 
+        if ('Items' in dist['Aliases']):
             for domain in dist['Aliases']['Items']:
                 print("\t" + domain)
         else:
@@ -51,27 +54,28 @@ def show_distributions():
                 print("\t" + domain['DomainName'])
         print("\n")
 
-    option = raw_input("\tQual cache deseja limpar? (ou 'q' para sair): ")
+    option = input("\tQual cache deseja limpar? (ou 'q' para sair): ")
     if option == 'q':
         print("\n")
         return
-    path = raw_input("\tCaminho do arquivo ou pattern: ")
+    path = input("\tCaminho do arquivo ou pattern: ")
 
     if len(dists) >= int(option):
         selected = dists[int(option)]
         print("\n\tCache Zerado: %s" % selected)
         invalidation = client.create_invalidation(DistributionId=selected,
-            InvalidationBatch={
-                'Paths': {
-                    'Quantity': 1,
-                    'Items': [path]
-            },
-            'CallerReference': str(time.time())
-        })
+                                                  InvalidationBatch={
+                                                      'Paths': {
+                                                          'Quantity': 1,
+                                                          'Items': [path]
+                                                      },
+                                                      'CallerReference': str(time.time())
+                                                  })
         print("\tStatus: %s" % invalidation['Invalidation']['Status'])
         print("\n")
     else:
         show_distributions()
+
 
 def menu():
     if choice == '1':
@@ -80,6 +84,7 @@ def menu():
         print("\n\tVolte sempre!")
     else:
         print("\n\tOpção Inválida.")
+
 
 display_title_bar()
 choice = get_user_choice()
